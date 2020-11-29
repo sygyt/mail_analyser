c61c7fbb1d0308d7f7f2791b1203dd8f88c91f33
Luca Debeir <l.debeir@me.com>
2020-11-29 18:03:20 +0100

    update top 10 fonction

--- Vectorizer.py
+++ Vectorizer.py
@@ -1,8 +1,8 @@
-
 import MailParser as mp
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+
 
 def top_tfidf_feats(row, features, top_n=20):
     topn_ids = np.argsort(row)[::-1][:top_n]
@@ -11,6 +11,19 @@ def top_tfidf_feats(row, features, top_n=20):
     return df
 
 
+# the top 10 terms in one document
 def top_feats_in_doc(X, features, row_id, top_n=25):
     row = np.squeeze(X[row_id].toarray())
     return top_tfidf_feats(row, features, top_n)
+
+
+# print the top terms across all documents
+def top_mean_feats(X, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    if grp_ids:
+        D = X[grp_ids].toarray()
+    else:
+        D = X.toarray()
+
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
+    return top_tfidf_feats(tfidf_means, features, top_n)
